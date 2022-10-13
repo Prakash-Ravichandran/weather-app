a6424b09fa6cf90d71d25d9498f26f2e599de14d
Prakash <prakashmtech1998@gmail.com>
2022-10-13 17:22:13 +0530

    added scrollTop logics

--- assets/js/script.js
+++ assets/js/script.js
@@ -319,3 +319,41 @@ function showCurrDay(dayString, dateString, element) {
       "rgba(0, 0, 0, 0.8)";
   }
 }
+
+// // feature for scrollTop
+// const scrollTop = function () {
+//   // create HTML button element
+//   console.log("inside scroll-Top function");
+//   const scrollBtn = document.createElement("button");
+//   scrollBtn.innerHTML = "&uarr;";
+//   scrollBtn.setAttribute("id", "scroll-btn");
+//   document.body.appendChild(scrollBtn);
+
+
+
+//   // hide/show button based on scroll distance
+//   const scrollBtnDisplay = function () {
+//     window.scrollY > window.innerHeight
+//       ? scrollBtn.classList.add("show")
+//       : scrollBtn.classList.remove("show");
+//   };
+
+
+//   window.addEventListener("scroll", scrollBtnDisplay);
+
+//   // scroll to top when button clicked
+//   const scrollWindow = function () {
+//     if (window.scrollY != 0) {
+//       setTimeout(function () {
+//         window.scrollTo(0, window.scrollY - 50);
+//         scrollWindow();
+//       }, 10);
+//     }
+//   };
+//   console.log("lastline scroll-Top function");
+//   scrollBtn.addEventListener("click", scrollWindow);
+// };
+// console.log("calling scroll-Top function");
+// scrollTop();
+// console.log("called scroll-Top function");
+
